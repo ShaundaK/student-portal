eec9a2c1f20fcff73b127eef18e347fa9a4f508c
Tea <tdinhla@protonmail.com>
2018-12-04 18:01:30 -0800

    add bootstrap cdn css & javascript, navbar in app.vue, and jumbotron for home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div class="jumbotron bg">
+      <h1 class="display-4">Welcome, students!</h1>
+      <p class="lead">Helping you keep track of your resume and capstone projects</p>
+      <p>Showcase your work to prospective employers</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Login</a>
+      </p>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          Keep track your web dev experiences
+        </div>
+        <div class="col-sm">
+          Showcase your capstone
+        </div>
+        <div class="col-sm">
+          Connect with other students
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style>
+.bg {
+  background: url('../assets/programmers-at-work.jpg') no-repeat center center;
+  width: 100%;
+  background-size: 35%;
+  background-position: right;
+}
+
+.jumbotron {
+  background-color: #e0e4ed;
+}
 </style>
 
 <script>
 export default {
   data: function() {
-    return {
-      message: "Welcome to Vue.js!"
-    };
+    return {};
   },
   created: function() {},
   methods: {},
