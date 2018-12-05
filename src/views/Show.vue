b5f727a8c8844583a588e66194c05fbdc8f527d6
ShaundaK <sdking1913@gmail.com>
2018-12-04 18:21:30 -0600

    create show router and student

---
+++ src/views/Show.vue
@@ -0,0 +1,33 @@
+<template>
+  <p> {{ student.first_name }} </p>
+  <p> {{ student.last_name }} </p>
+  <p> {{ student.email }} </p>
+  <p> {{ student.phone_number }} </p>
+  <p> {{ student.short_bio }} </p>
+  <p> {{ student.linkedin_url }} </p>
+  <p> {{ student.personal_blog_url }}</p>
+  <p> {{ student.online_resume_url }}</p>
+  <p> {{ student.github_url }}</p>
+  <p> {{ student.photo_url }}</p>
+</template>
+
+<script>
+var axios = require('axios');
+export default {
+  data: function() {
+    return {
+      message: "Welcome to the show componenentt",
+      student: {}
+    };
+  },
+  created: function() {
+    console.log(this);
+    axios.get('http://localhost:3000/api/students/' + this.$route.params.id).then(response => {
+      console.log(response.data);
+      this.student = response.data;
+    })
+  },
+  methods: {},
+  computed: {}
+};
+</script>
